384ebaa10283dba6484c616682ed49264752262f
gasparyandav <dgasparyan29@gmail.com>
2023-10-10 17:12:54 +0400

    users-obj

---
+++ lesson1.js
@@ -0,0 +1,45 @@
+const users=[
+    {
+      "_id": "651d5854a0739766c6accd95",
+      "balance": "$2,870.53",
+      "age": 38,
+      "name": "Brady Travis"
+    },
+    {
+      "_id": "651d5854ebcfd194ddaf1b59",
+      "balance": "$1,840.75",
+      "age": 68,
+      "name": "Mooney Stone"
+    },
+    {
+      "_id": "651d58542a36694c4e813fd3",
+      "balance": "$2,726.08",
+      "age": 34,
+      "name": "Ferrell Santos"
+    },
+    {
+      "_id": "651d58545e65516564b27732",
+      "balance": "$3,553.30",
+      "age": 53,
+      "name": "Benita Bell"
+    },
+    {
+      "_id": "651d5854d03eafdffb24ee5f",
+      "balance": "$2,535.63",
+      "age": 51,
+      "name": "Griffith Weaver"
+    },
+    {
+      "_id": "651d58543dfe2f0111aefc96",
+      "balance": "$3,492.23",
+      "age": 58,
+      "name": "Letha Dunn"
+    },
+    {
+      "_id": "651d58543de965a7954837d4",
+      "balance": "$3,732.45",
+      "age": 19,
+      "name": "Garrison Rasmussen"
+    }
+  ]
+  
